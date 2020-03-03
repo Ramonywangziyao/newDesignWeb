e0ccd3abc9ed1606233bc438d0b0c896168f972f
Ziyao Wang <tonywangziyao@gmail.com>
2020-03-03 18:24:17 -0500

    Update emailTo.php

--- emailTo.php
+++ emailTo.php
@@ -5,7 +5,7 @@
   $emailFrom = 'tonywangziyao@gmail.com';
   $subject = 'New Request from Customer';
   $emailContent = 'A new request by a customer\nEmail: $responEmail \nMessage: $message\n';
-  $toEmail = 'tonywangziyao@gmail.com';
+  $toEmail = '';
   $header = 'From:NOUS Studio Request\r\n';
   mail($toEmail,$subject,$emailContent,$header);
   echo "sucess";
